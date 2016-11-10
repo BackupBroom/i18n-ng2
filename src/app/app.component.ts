8b50d4c125d1a912fc7a1855d40e8482563f49a0
ES\Ekaterina.Bazarnova <ekaterina.bazarnova@effective-soft.com>
2016-11-10 23:34:03 +0300

    Exploration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
 
+class LocaleViewObject {
+
+  code: String;
+  displayName: String;
+
+  constructor (code: String, displayName: String) {
+    this.code = code.toLowerCase();
+    this.displayName = displayName;
+  }
+
+}
+
+const locales = [
+  new LocaleViewObject ('en', 'English'),
+  new LocaleViewObject ('ru', 'Russian'),
+  new LocaleViewObject ('de', 'German')
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
+
 export class AppComponent {
-  title = 'app works!';
+
+  title = 'Some String to Translate';
+  activeLocale = locales[0];
+
+  getLanguages(): Array<LocaleViewObject> {
+    return locales;
+  };
+
+  onLocaleSelected(code: String): void {
+    const activeLocale = locales.find(findLocaleByCode);
+    this.activeLocale = activeLocale;
+
+    function findLocaleByCode (locale) {
+      return locale.code === code;
+    }
+  }
 }
